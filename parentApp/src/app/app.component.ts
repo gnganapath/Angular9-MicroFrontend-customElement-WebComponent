eef793228888d7da1c042fa54f83e374dabe7290
gnganpath <esourcej@gmail.com>
2020-04-19 00:12:35 +0530

    Master app Complet set and updated conent from micro App

--- parentApp/src/app/app.component.ts
+++ parentApp/src/app/app.component.ts
@@ -1,10 +1,42 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.less']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'parentApp';
+
+  
+  childAppPath = 'http://localhost:4200/assets/childApp/main.js';
+  constructor(){
+
+
+  }
+
+  ngOnInit() {
+    this.loadScript(this.childAppPath);
+   
+  }
+
+  loadScript(scriptPath): void {
+
+    var alreadyLoaded = false;
+
+    for (var scriptIndex in document.scripts) {
+     // console.log(scriptPath, scriptIndex, document.scripts[scriptIndex].src)
+      if (!alreadyLoaded && scriptPath === document.scripts[scriptIndex].src) {
+        alreadyLoaded = true;
+        break;
+      }
+    }
+    if (!alreadyLoaded) {
+      const content = document.getElementById('content');
+      const script = document.createElement('script');
+      script.src = scriptPath;
+      content.appendChild(script);
+    }
+
+  }
 }
